4368d97253f7e701989310cff245ee116995eb36
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-09-20 14:51:18 +0530

    Update formatters.py

--- SakkuMusic/utils/formatters.py
+++ SakkuMusic/utils/formatters.py
@@ -6,7 +6,7 @@ def get_readable_time(seconds: int) -> str:
     count = 0
     ping_time = ""
     time_list = []
-    time_suffix_list = ["s", "ᴍ", "ʜ", "ᴅᴀʏs"]
+    time_suffix_list = ["s", "m", "h", "days"]
     while count < 4:
         count += 1
         if count < 3:
