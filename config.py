d84ad40ead89920d741f91adf34d56ddf36e0c98
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-09-20 14:06:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/deepaiimsss/SakkuMusic",
+    "https://github.com/deepaiimsss/SakkuMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
