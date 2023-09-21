dfe0effb0f312ba12955bc47dc040700e2bef4f9
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-09-21 11:35:54 +0530

    Create active.py

---
+++ SakkuMusic/plugins/tools/active.py
@@ -0,0 +1,76 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from unidecode import unidecode
+
+from SakkuMusic import app
+from SakkuMusic.misc import SUDOERS
+from SakkuMusic.utils.database import (
+    get_active_chats,
+    get_active_video_chats,
+    remove_active_chat,
+    remove_active_video_chat,
+)
+
+
+@app.on_message(filters.command(["activevc", "activevoice"]) & SUDOERS)
+async def activevc(_, message: Message):
+    mystic = await message.reply_text("Getting active voice chats.. Please hold")
+    served_chats = await get_active_chats()
+    text = ""
+    j = 0
+    for x in served_chats:
+        try:
+            title = (await app.get_chat(x)).title
+        except:
+            await remove_active_chat(x)
+            continue
+        try:
+            if (await app.get_chat(x)).username:
+                user = (await app.get_chat(x)).username
+                text += f"<b>{j + 1}.</b> <a href=https://t.me/{user}>{unidecode(title).upper()}</a> [<code>{x}</code>]\n"
+            else:
+                text += (
+                    f"<b>{j + 1}.</b> {unidecode(title).upper()} [<code>{x}</code>]\n"
+                )
+            j += 1
+        except:
+            continue
+    if not text:
+        await mystic.edit_text(f"No Active Voice Chats{app.mention}.")
+    else:
+        await mystic.edit_text(
+            f"<b>Active Voice Chats:-</b>\n\n{text}",
+            disable_web_page_preview=True,
+        )
+
+
+@app.on_message(filters.command(["activev", "activevideo"]) & SUDOERS)
+async def activevi_(_, message: Message):
+    mystic = await message.reply_text("Getting active voice chats.. Please hold")
+    served_chats = await get_active_video_chats()
+    text = ""
+    j = 0
+    for x in served_chats:
+        try:
+            title = (await app.get_chat(x)).title
+        except:
+            await remove_active_video_chat(x)
+            continue
+        try:
+            if (await app.get_chat(x)).username:
+                user = (await app.get_chat(x)).username
+                text += f"<b>{j + 1}.</b> <a href=https://t.me/{user}>{unidecode(title).upper()}</a> [<code>{x}</code>]\n"
+            else:
+                text += (
+                    f"<b>{j + 1}.</b> {unidecode(title).upper()} [<code>{x}</code>]\n"
+                )
+            j += 1
+        except:
+            continue
+    if not text:
+        await mystic.edit_text(f"No Active Voice Chats{app.mention}.")
+    else:
+        await mystic.edit_text(
+            f"<b>Active Video Calls:-</b>\n\n{text}",
+            disable_web_page_preview=True,
+        )
