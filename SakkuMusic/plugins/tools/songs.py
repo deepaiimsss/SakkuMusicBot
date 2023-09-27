0c315cbeb3f43a332cbc915f800dab9e5732e2ba
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-09-27 11:46:25 +0530

    Update songs.py

--- SakkuMusic/plugins/tools/songs.py
+++ SakkuMusic/plugins/tools/songs.py
@@ -258,8 +258,7 @@ async def song_download_cb(client, CallbackQuery, _):
         )
         await mystic.edit_text(_["song_11"])
         await app.send_chat_action(
-            chat_id=CallbackQuery.message.chat.id,
-            action="upload_video",
+            chat_id=CallbackQuery.message.chat.id,ChatAction.upload_video
         )
         try:
             await CallbackQuery.edit_message_media(media=med)
@@ -287,8 +286,7 @@ async def song_download_cb(client, CallbackQuery, _):
         )
         await mystic.edit_text(_["song_11"])
         await app.send_chat_action(
-            chat_id=CallbackQuery.message.chat.id,
-            action="upload_audio",
+            chat_id=CallbackQuery.message.chat.id,ChatAction.upload_audio
         )
         try:
             await CallbackQuery.edit_message_media(media=med)
