6decc0a558eece2df410b31b9e691fd0f84475d0
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-09-27 13:41:20 +0530

    Update songs.py

--- SakkuMusic/plugins/tools/songs.py
+++ SakkuMusic/plugins/tools/songs.py
@@ -3,6 +3,7 @@ import re
 
 import yt_dlp
 from pykeyboard import InlineKeyboard
+from pyrogram import enums
 from pyrogram.enums import ChatAction
 from pyrogram import filters
 from pyrogram.types import (InlineKeyboardButton,
